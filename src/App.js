20bab7bdf9dbbbc41b32fb21d278bde9e7fea22c
yaminmhd <mdyamin.drzee@gmail.com>
2018-09-30 00:20:15 +0800

    first step

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-import TotalScore from './TotalScore'
+import React, { Component } from "react";
+import "./App.css";
+import TotalScore from "./TotalScore";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  decrement() {
+    this.setState(state => {
+      return {
+        count: state.count - 1
+      };
+    });
+  }
+
+  increment() {
+    this.setState(state => {
+      return {
+        count: state.count + 1
+      };
+    });
+  }
+
   render() {
+    const {name} = this.props;
     return (
-      <TotalScore/>
+      <div>
+        <h1 style={{textAlign: 'center'}}>{name}</h1>
+        <div id="main-counter">
+          <button onClick={this.decrement.bind(this)}>-</button>
+          {this.state.count}
+          <button onClick={this.increment.bind(this)}>+</button>
+        </div>
+      </div>
     );
   }
 }
